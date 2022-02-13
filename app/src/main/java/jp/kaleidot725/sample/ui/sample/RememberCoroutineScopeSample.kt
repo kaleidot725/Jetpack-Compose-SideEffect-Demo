9c979835c18fb41cc81451eedfccd50eeb38e3bc
kaleidot725 <hakodate.katsuragawa.yusuke@gmail.com>
2022-02-13 17:19:26 +0900

    feat: Update rememberCoroutineScope

--- app/src/main/java/jp/kaleidot725/sample/ui/sample/RememberCoroutineScopeSample.kt
+++ app/src/main/java/jp/kaleidot725/sample/ui/sample/RememberCoroutineScopeSample.kt
@@ -1,5 +1,6 @@
 package jp.kaleidot725.sample.ui.sample
 
+import android.content.Context
 import android.widget.Toast
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -13,21 +14,23 @@ import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.unit.dp
 import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.cancel
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
 
 
 @Composable
 fun RememberCoroutineScopeSample() {
-    val count = rememberSaveable { mutableStateOf(0f) }
+    val count = remember { mutableStateOf(0f) }
     val scope = rememberCoroutineScope()
+    val context = LocalContext.current
 
     Box(modifier = Modifier.fillMaxSize()) {
         Column(
@@ -45,7 +48,7 @@ fun RememberCoroutineScopeSample() {
             )
 
             Button(
-                onClick = { increment(scope, count) },
+                onClick = { increment(context, scope, count) },
                 modifier = Modifier
                     .fillMaxWidth()
                     .padding(8.dp)
@@ -54,7 +57,7 @@ fun RememberCoroutineScopeSample() {
             }
 
             Button(
-                onClick = { decrement(scope, count) },
+                onClick = { decrement(context, scope, count) },
                 modifier = Modifier
                     .fillMaxWidth()
                     .padding(8.dp)
@@ -65,20 +68,24 @@ fun RememberCoroutineScopeSample() {
     }
 }
 
-private fun increment(scope: CoroutineScope, count: MutableState<Float>) {
+private fun increment(context: Context, scope: CoroutineScope, count: MutableState<Float>) {
     scope.launch {
+        Toast.makeText(context, "START INCREMENT", Toast.LENGTH_SHORT).show()
         repeat(10) {
-            delay(50)
+            delay(300)
             count.value = count.value + 0.1f
         }
+        Toast.makeText(context, "END INCREMENT", Toast.LENGTH_SHORT).show()
     }
 }
 
-private fun decrement(scope: CoroutineScope, count: MutableState<Float>) {
+private fun decrement(context: Context, scope: CoroutineScope, count: MutableState<Float>) {
     scope.launch {
+        Toast.makeText(context, "START DECREMENT", Toast.LENGTH_SHORT).show()
         repeat(10) {
-            delay(50)
+            delay(300)
             count.value = count.value - 0.1f
         }
+        Toast.makeText(context, "END DECREMENT", Toast.LENGTH_SHORT).show()
     }
 }

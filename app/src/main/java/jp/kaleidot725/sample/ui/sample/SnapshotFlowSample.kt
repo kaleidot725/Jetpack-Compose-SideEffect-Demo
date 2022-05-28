dea3528737f610208be407b726c0fcab82553ef9
kaleidot725 <hakodate.katsuragawa.yusuke@gmail.com>
2022-05-28 14:38:23 +0900

    Update SnapshotFlowSample.kt

--- app/src/main/java/jp/kaleidot725/sample/ui/sample/SnapshotFlowSample.kt
+++ app/src/main/java/jp/kaleidot725/sample/ui/sample/SnapshotFlowSample.kt
@@ -1,7 +1,10 @@
 package jp.kaleidot725.sample.ui.sample
 
-import android.util.Log
-import androidx.compose.foundation.layout.*
+import android.widget.Toast
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.rememberLazyListState
 import androidx.compose.material.Card
@@ -10,35 +13,31 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.snapshotFlow
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.unit.dp
 import kotlinx.coroutines.flow.collect
 
 @Composable
 fun SnapshotFlowSample() {
-    val listState = rememberLazyListState()
+    val lazyListState = rememberLazyListState()
+    val context = LocalContext.current
 
-    LaunchedEffect(listState) {
-        snapshotFlow {
-            Log.v("TEST", "Enter")
-            listState.firstVisibleItemIndex
-        }.collect {
-            Log.v("TEST", "Collect FirstVisibleItemIndex $it")
+    LaunchedEffect(lazyListState) {
+        val firstVisibleItemIndexFlow = snapshotFlow { lazyListState.firstVisibleItemIndex }
+        firstVisibleItemIndexFlow.collect { firstVisibleItemIndex ->
+            Toast.makeText(context, "Update $firstVisibleItemIndex", Toast.LENGTH_SHORT).show()
         }
     }
 
     LazyColumn(
-        state = listState,
+        state = lazyListState,
         verticalArrangement = Arrangement.spacedBy(10.dp),
         modifier = Modifier.padding(8.dp)
     ) {
-        repeat(10) {
+        repeat(10) { number ->
             item {
-                Card(
-                    modifier = Modifier
-                        .fillMaxSize()
-                        .height(100.dp)
-                ) {
-                    Text(text = it.toString())
+                Card(modifier = Modifier.fillMaxSize().height(100.dp)) {
+                    Text(text = "$number")
                 }
             }
         }

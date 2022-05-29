e3cfcdf2f4486dc77ec91a98073ce5ba5fbbddff
kaleidot725 <hakodate.katsuragawa.yusuke@gmail.com>
2022-05-29 15:32:23 +0900

    Update

--- app/src/main/java/jp/kaleidot725/sample/ui/sample/DerivedStateOfSample.kt
+++ app/src/main/java/jp/kaleidot725/sample/ui/sample/DerivedStateOfSample.kt
@@ -1,111 +1,59 @@
 package jp.kaleidot725.sample.ui.sample
 
-import android.util.Log
+import android.widget.Toast
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
-import androidx.compose.material.*
+import androidx.compose.material.Button
+import androidx.compose.material.Card
+import androidx.compose.material.Text
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.unit.dp
-import jp.kaleidot725.sample.ui.sample.News.Companion.SAMPLES
 import java.util.*
 
-enum class FilterType {
-    PINNED,
-    UNPINNED,
-    NONE,
+private val SAMPLES = listOf(
+    News("ONE", "ONE ONE ONE", News.Type.BUSINESS),
+    News("TWO", "TWO TWO TWO", News.Type.BUSINESS),
+    News("THREE", "THREE THREE THREE", News.Type.SPORTS),
+    News("FOUR", "FOUR FOUR FOUR", News.Type.HEALTH),
+    News("FIVE", "FIVE FIVE FIVE", News.Type.HEALTH),
+)
+
+private data class News(val title: String, val content: String, val type: Type) {
+    enum class Type {
+        BUSINESS, SPORTS, HEALTH
+    }
 }
 
-private data class News(
-    val title: String,
-    val content: String,
-    val pinned: Boolean
-) {
-    companion object {
-        val SAMPLES = listOf(
-            News("ONE", "ONE ONE ONE", true),
-            News("TWO", "TWO TWO TWO", true),
-            News("THREE", "THREE THREE THREE", false),
-            News("FOUR", "FOUR FOUR FOUR", false),
-            News("FIVE", "FIVE FIVE FIVE", false),
+@Composable
+fun DerivedStateOfSample() {
+    val newsList by remember { mutableStateOf(SAMPLES) }
+    var filterType by remember { mutableStateOf(News.Type.BUSINESS) }
+    var refreshTime by remember { mutableStateOf(Date().time) }
+
+    Box(modifier = Modifier
+        .fillMaxSize()
+        .padding(8.dp)) {
+        FilteredNewsList(refreshTime, newsList, filterType)
+        FilterMenus(
+            onFilterBusiness = { filterType = News.Type.BUSINESS },
+            onFilterSports = { filterType = News.Type.SPORTS },
+            onFilterHealth = { filterType = News.Type.HEALTH },
+            onRefresh = { refreshTime = Date().time }
         )
     }
 }
 
 @Composable
-private fun NewsCard(news: News, onChangeStatus: () -> Unit, modifier: Modifier = Modifier) {
-    Card(modifier) {
-        Column {
-            Text(text = news.title)
-            Text(text = news.content)
-            Button(onClick = { onChangeStatus() }, modifier = Modifier.fillMaxWidth()) {
-                Text(text = if (news.pinned) "UNPIN" else "PIN")
-            }
-        }
-    }
-}
-
-@Preview
-@Composable
-private fun NewsCard_Preview() {
-    NewsCard(news = SAMPLES.first(), onChangeStatus = {}, modifier = Modifier.fillMaxSize())
-}
-
-@Composable
-fun DerivedStateOfSample() {
-    var filterType by remember { mutableStateOf(FilterType.NONE) }
-    var refreshTime by remember { mutableStateOf(Date().time) }
-
-    Box(modifier = Modifier.fillMaxSize()) {
-        NewsList(refreshTime, filterType)
-
-        Column(modifier = Modifier.align(Alignment.BottomEnd)) {
-            FloatingActionButton(
-                onClick = {
-                    filterType = when (filterType) {
-                        FilterType.PINNED -> FilterType.UNPINNED
-                        FilterType.UNPINNED -> FilterType.NONE
-                        FilterType.NONE -> FilterType.PINNED
-                    }
-                }
-            ) {
-                Text(text = "Change")
-            }
-
-            FloatingActionButton(
-                onClick = { refreshTime = Date().time }
-            ) {
-                Text(text = "Refresh")
-            }
-        }
-    }
-}
-
-@Composable
-private fun NewsList(refreshTime: Long, filterType: FilterType) {
-    Log.v("TEST", "enter pinned news list")
-
-    var newsList by remember { mutableStateOf(SAMPLES) }
-
-//    Log.v("TEST", "calc pinned news list")
-//    val filteredList = when (filterType) {
-//        FilterType.PINNED -> newsList.filter { it.pinned }
-//        FilterType.UNPINNED -> newsList.filter { !it.pinned }
-//        FilterType.NONE -> newsList
-//    }
-
-    val filteredList by remember(filterType) {
+private fun BoxScope.FilteredNewsList(refreshTime: Long, newsList: List<News>, type: News.Type) {
+    val context = LocalContext.current
+    val filteredList by remember(newsList, type) {
         derivedStateOf {
-            Log.v("TEST", "calc pinned news list")
-            when (filterType) {
-                FilterType.PINNED -> newsList.filter { it.pinned }
-                FilterType.UNPINNED -> newsList.filter { !it.pinned }
-                FilterType.NONE -> newsList
-            }
+            Toast.makeText(context, "calculation filtered news list", Toast.LENGTH_SHORT).show()
+            newsList.filter { it.type == type }
         }
     }
 
@@ -114,31 +62,56 @@ private fun NewsList(refreshTime: Long, filterType: FilterType) {
         verticalArrangement = Arrangement.spacedBy(8.dp),
     ) {
         item {
-            Surface(color = Color.LightGray, modifier = Modifier.fillMaxWidth()) {
-                Text(
-                    text = "${filterType.name} $refreshTime",
-                    style = MaterialTheme.typography.h6
-                )
-            }
+            Text(text = "REFRESH TIME $refreshTime")
         }
 
         items(filteredList) { news ->
-            NewsCard(
-                news = news,
-                onChangeStatus = {
-                    newsList = buildList {
-                        addAll(newsList)
-                        val index = newsList.indexOf(news)
-                        val newNews = news.copy(pinned = !news.pinned)
-                        remove(news)
-                        add(index, newNews)
-                    }
-                },
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .height(100.dp)
-                    .padding(horizontal = 4.dp)
-            )
+            Card(modifier = Modifier
+                .fillMaxWidth()
+                .height(100.dp)) {
+                Column {
+                    Text(text = news.type.toString())
+                    Text(text = news.title)
+                    Text(text = news.content)
+                }
+            }
+        }
+    }
+}
+
+@Composable
+private fun BoxScope.FilterMenus(
+    onFilterBusiness: () -> Unit,
+    onFilterSports: () -> Unit,
+    onFilterHealth: () -> Unit,
+    onRefresh: () -> Unit
+) {
+    Column(
+        modifier = Modifier.align(Alignment.BottomEnd),
+        verticalArrangement = Arrangement.spacedBy(8.dp)
+    ) {
+        Button(
+            onClick = { onFilterBusiness() }, modifier = Modifier.fillMaxWidth()
+        ) {
+            Text(text = "BUSINESS")
+        }
+
+        Button(
+            onClick = { onFilterSports() }, modifier = Modifier.fillMaxWidth()
+        ) {
+            Text(text = "SPORTS")
+        }
+
+        Button(
+            onClick = { onFilterHealth() }, modifier = Modifier.fillMaxWidth()
+        ) {
+            Text(text = "HEALTH")
+        }
+
+        Button(
+            onClick = { onRefresh() }, modifier = Modifier.fillMaxWidth()
+        ) {
+            Text(text = "REFRESH")
         }
     }
 }
